2219abf0090f8f0f0f2dbe38a4bfe0d42d4bf048
Agustina Mannise <agusmn95@gmail.com>
2016-01-19 17:22:29 -0300

    Nueva clase enumerada

---
+++ TP-Final/TP-Final/DiaSemana.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Final
+{
+    public enum DiaSemana
+    {
+        Lunes,
+        Martes,
+        Miercoles,
+        Jueves,
+        Viernes,
+        Sabado,
+        Domingo
+    }
+}
