0336d3c20985cefb2571f0f667a03e33c8a83340
Agustina Mannise <agusmn95@gmail.com>
2016-01-20 15:30:25 -0300

    Banner Estático

--- TP-Final/TP-Final/Banner.cs
+++ TP-Final/TP-Final/Banner.cs
@@ -6,8 +6,35 @@ using System.Threading.Tasks;
 
 namespace TP_Final
 {
-    class Banner
+    public class Banner
     {
-        public int numero;
+        private IList<Horario> iFrecuencia;
+        private DateTime iFechaInicio;
+        private DateTime iFechaFin;
+
+        public IList<Horario> Frecuencia
+        {
+            get { return iFrecuencia; }
+            set { iFrecuencia = value; }
+        }
+
+        public DateTime FechaInicio
+        {
+            get { return iFechaInicio; }
+            set { iFechaInicio = value; }
+        }
+
+        public DateTime FechaFin
+        {
+            get { return iFechaFin; }
+            set { iFechaFin = value; }
+        }
+
+        public Banner ()
+        {
+            iFrecuencia = new List<Horario>();
+            iFechaFin = new DateTime();
+            iFechaInicio = new DateTime();
+        }
     }
 }
---
+++ TP-Final/TP-Final/BannerEstatico.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Final
+{
+    public class BannerEstatico : Banner
+    {
+        private string iTexto;
+
+        public string Texto
+        {
+            get { return iTexto; }
+            set { iTexto = value; }
+        }
+
+        public BannerEstatico (string pTexto)
+        {
+            this.Texto = pTexto;
+        }
+    }
+}
