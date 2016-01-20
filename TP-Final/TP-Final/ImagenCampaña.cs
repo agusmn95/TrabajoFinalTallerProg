72e9e544cd6eccfb793c3bf192df809e18db896e
Valentín Lazbal <davidlazbal@hotmail.com>
2016-01-20 17:56:23 -0300

    ImagenCampaña 20/01

--- TP-Final/TP-Final/ImagenCampaña.cs
+++ TP-Final/TP-Final/ImagenCampaña.cs
@@ -1,6 +1,29 @@
-﻿namespace TP_Final
+﻿using System.Drawing;
+using System;
+namespace TP_Final
 {
-    internal class ImagenCampaña
+    public class ImagenCampaña
     {
+        private string iRuta;
+        private Image iImagen;
+
+        //Constructor
+        public ImagenCampaña(string pRuta)
+        {
+            iRuta = pRuta;
+            iImagen = Image.FromFile(iRuta);
+        }
+
+        //Propiedades
+        public string Ruta
+        {
+            get { return iRuta; }
+            set
+            {
+                iRuta = value;
+                iImagen = Image.FromFile(iRuta);
+            }
+        }
+        public Image Imagen { get { return iImagen; } }
     }
 }
